998db764b4de9f914d243ce4bb4ab18c7598d1eb
Vivian Azalia <55457999+vivianazalia@users.noreply.github.com>
2021-12-31 01:19:26 +0700

    Add project

---
+++ Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Player 
+{
+	public class Login
+	{
+		public struct LoginInfo
+		{
+			string username;
+			int id;
+		}
+	}
+
+	public class Player
+	{
+		LoginInfo info;
+
+		public void Play()
+		{
+			LoginInfo infoLogin = new LoginInfo { username = "Aaa", id = 1 };
+			Player player = new Player { info = infoLogin }
+		}
+	}
+}
+
+
